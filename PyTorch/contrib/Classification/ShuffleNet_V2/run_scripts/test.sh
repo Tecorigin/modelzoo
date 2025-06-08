2239b49eab050478210d0c01396d29ac1bab896f
root <root@nb-lxhmbaxevc-0.nbc-rohvolzzwh.kubecube-workspace-606.svc.cluster.local>
2025-06-08 12:32:49 +0000

    v2

---
+++ PyTorch/contrib/Classification/ShuffleNet_V2/run_scripts/test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+script_path=$(dirname $(readlink -f "$0"))
+echo "当前脚本路径: $script_path"
+
+# 添加 PYTHONPATH，确保能找到 mmpretrain 模块
+# 自动推导 mmpretrain 模块所在路径
+export PYTHONPATH=$(realpath "$script_path/../mmpretrain"):$PYTHONPATH
+
+# 安装依赖
+echo "正在安装Python依赖..."
+### 安装环境的 交上去的时候取消注释 下面三行
+# cd $script_path/../
+# pip install -r requirements.txt
+# python setup.py develop --no_cuda_ext
+
+# 数据集路径,保持默认统一根目录即可
+data_path="/data/teco-data/imagenet"
+
+# 训练参数配置
+config_file="$script_path/shufflenet_v2/shufflenet-v2-1x_16xb64_in1k.py"
+log_file="$script_path/sdaa.log"
+cuda_log_file="$script_path/cuda.log"
+train_file="$script_path/run_shufflenet_v2.py"
+
+# 启动训练
+echo "开始训练..."
+cd $script_path/
+#跑的命令
+torchrun  \
+      --master_port=29500 \
+      $train_file \
+      $config_file \
+      --launcher pytorch \
+      --amp | tee sdaa.log
+
+# 生成loss曲线图
+echo "生成训练结果图表..."
+python loss.py \
+    --sdaa-log $log_file \
+    --cuda-log $cuda_log_file
+
+echo "训练完成！结果保存在:"
+echo " - 训练日志: $log_file"
+echo " - Loss曲线: $script_path/loss.jpg"
---
+++ PyTorch/contrib/Classification/ShuffleNet_V2/run_scripts/work_dirs/shufflenet-v2-1x_16xb64_in1k/shufflenet-v2-1x_16xb64_in1k.py
@@ -0,0 +1,138 @@
+auto_scale_lr = dict(base_batch_size=1024)
+data_preprocessor = dict(
+    mean=[
+        123.675,
+        116.28,
+        103.53,
+    ],
+    num_classes=1000,
+    std=[
+        58.395,
+        57.12,
+        57.375,
+    ],
+    to_rgb=True)
+dataset_type = 'ImageNet'
+default_hooks = dict(
+    checkpoint=dict(interval=1, type='CheckpointHook'),
+    logger=dict(interval=100, type='LoggerHook'),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    timer=dict(type='IterTimerHook'),
+    visualization=dict(enable=False, type='VisualizationHook'))
+default_scope = 'mmpretrain'
+env_cfg = dict(
+    cudnn_benchmark=False,
+    dist_cfg=dict(backend='nccl'),
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0))
+launcher = 'pytorch'
+load_from = None
+log_level = 'INFO'
+model = dict(
+    backbone=dict(type='ShuffleNetV2', widen_factor=1.0),
+    head=dict(
+        in_channels=1024,
+        loss=dict(loss_weight=1.0, type='CrossEntropyLoss'),
+        num_classes=1000,
+        topk=(
+            1,
+            5,
+        ),
+        type='LinearClsHead'),
+    neck=dict(type='GlobalAveragePooling'),
+    type='ImageClassifier')
+optim_wrapper = dict(
+    loss_scale='dynamic',
+    optimizer=dict(lr=0.5, momentum=0.9, type='SGD', weight_decay=4e-05),
+    paramwise_cfg=dict(norm_decay_mult=0),
+    type='AmpOptimWrapper')
+param_scheduler = [
+    dict(begin=0, by_epoch=False, end=5000, factor=0.1, type='ConstantLR'),
+    dict(begin=5000, by_epoch=False, eta_min=0, type='PolyLR'),
+]
+randomness = dict(deterministic=False, seed=None)
+resume = False
+test_cfg = dict()
+test_dataloader = dict(
+    batch_size=64,
+    collate_fn=dict(type='default_collate'),
+    dataset=dict(
+        data_root='/data/teco-data/imagenet',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(backend='pillow', edge='short', scale=256, type='ResizeEdge'),
+            dict(crop_size=224, type='CenterCrop'),
+            dict(type='PackInputs'),
+        ],
+        split='val',
+        type='ImageNet'),
+    num_workers=5,
+    persistent_workers=True,
+    pin_memory=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+test_evaluator = dict(
+    topk=(
+        1,
+        5,
+    ), type='Accuracy')
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(backend='pillow', edge='short', scale=256, type='ResizeEdge'),
+    dict(crop_size=224, type='CenterCrop'),
+    dict(type='PackInputs'),
+]
+train_cfg = dict(by_epoch=True, max_epochs=300, val_interval=1)
+train_dataloader = dict(
+    batch_size=64,
+    collate_fn=dict(type='default_collate'),
+    dataset=dict(
+        data_root='/data/teco-data/imagenet',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(backend='pillow', scale=224, type='RandomResizedCrop'),
+            dict(direction='horizontal', prob=0.5, type='RandomFlip'),
+            dict(type='PackInputs'),
+        ],
+        split='train',
+        type='ImageNet'),
+    num_workers=5,
+    persistent_workers=True,
+    pin_memory=True,
+    sampler=dict(shuffle=True, type='DefaultSampler'))
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(backend='pillow', scale=224, type='RandomResizedCrop'),
+    dict(direction='horizontal', prob=0.5, type='RandomFlip'),
+    dict(type='PackInputs'),
+]
+val_cfg = dict()
+val_dataloader = dict(
+    batch_size=64,
+    collate_fn=dict(type='default_collate'),
+    dataset=dict(
+        data_root='/data/teco-data/imagenet',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(backend='pillow', edge='short', scale=256, type='ResizeEdge'),
+            dict(crop_size=224, type='CenterCrop'),
+            dict(type='PackInputs'),
+        ],
+        split='val',
+        type='ImageNet'),
+    num_workers=5,
+    persistent_workers=True,
+    pin_memory=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+val_evaluator = dict(
+    topk=(
+        1,
+        5,
+    ), type='Accuracy')
+vis_backends = [
+    dict(type='LocalVisBackend'),
+]
+visualizer = dict(
+    type='UniversalVisualizer', vis_backends=[
+        dict(type='LocalVisBackend'),
+    ])
+work_dir = './work_dirs/shufflenet-v2-1x_16xb64_in1k'
